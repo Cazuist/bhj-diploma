69998fa2a4c2267e7196a5c5122978b6a83a8436
Cazuist <sovrus@mail.ru>
2020-06-03 18:54:58 +0300

    Entity. First commit

--- public/js/api/Entity.js
+++ public/js/api/Entity.js
@@ -1,41 +1,50 @@
-/**
- * Класс Entity - базовый для взаимодействия с сервером.
- * Имеет свойство URL, равно пустой строке.
- * */
+'use strict';
+
 class Entity {
 
-  /**
-   * Запрашивает с сервера список данных.
-   * Это могут быть счета или доходы/расходы
-   * (в зависимости от того, что наследуется от Entity)
-   * */
-  static list( data, callback = f => f ) {
+  static URL = '';
 
+  static list( data = User.current(), callback = f => f ) {
+    const options = {
+      data,
+      url: this.URL,
+      method: 'GET',
+      callback, 
+    }
+    
+    return createRequest(options);
   }
 
-  /**
-   * Создаёт счёт или доход/расход с помощью запроса
-   * на сервер. (в зависимости от того,
-   * что наследуется от Entity)
-   * */
   static create( data, callback = f => f ) {
+    const options = {
+      data: Object.assign({_method: 'PUT'}, data),
+      url: this.URL,
+      method: 'POST',
+      callback,  
+    }
 
+    return createRequest(options);
   }
 
-  /**
-   * Получает информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static get( id = '', data, callback = f => f ) {
+    const options = {
+      data: Object.assign({id}, {_method: 'DELETE'}, data),
+      url: this.URL,
+      method: 'GET',
+      callback, 
+    }
 
+    return createRequest(options);
   }
 
-  /**
-   * Удаляет информацию о счёте или доходе/расходе
-   * (в зависимости от того, что наследуется от Entity)
-   * */
   static remove( id = '', data, callback = f => f ) {
+    const options = {
+      data: Object.assign({id}, {_method: 'DELETE'}, data),
+      url: this.URL,
+      method: 'POST',
+      callback,
+    }
 
+    return createRequest(options);
   }
 }
-

5582a4978f19edc0448ad34f1cab0b51ac902a82
Cazuist <sovrus@mail.ru>
2020-06-04 09:05:23 +0300

    Modal. First addition

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,54 +1,43 @@
-/**
- * Класс Modal отвечает за
- * управление всплывающими окнами.
- * В первую очередь это открытие или
- * закрытие имеющихся окон
- * */
+'use strict';
+
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью
-   * AccountsWidget.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
+  
   constructor( element ) {
+    if(!element) {
+      throw new Error('Не могу создать что-то из ничего');
+    }
 
+    this.element = element;
+    this.registerEvents();
   }
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
   registerEvents() {
+    const dismiss = Array.from( this.element.querySelectorAll('[data-dismiss=modal]') );
 
+    dismiss.forEach( (item) => {
+      item.addEventListener( 'click', this.onClose() );
+    });
   }
 
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
-  onClose( e ) {
-
+  onClose(event) {    
+    return () => {
+      this.close();
+      };    
   }
-  /**
-   * Удаляет обработчики событий
-   * */
+ 
   unregisterEvents() {
-
+    const dismiss = Array.from( this.element.querySelectorAll('[data-dismiss=modal]') );
+    dismiss.forEach( (item) => {
+      item.removeEventListener( 'click', this.onClose() );
+    });
   }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
+
   open() {
-
+    this.element.style.display = 'block';
   }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-
+ 
+  close() {
+    this.element.style.display = 'none';
+    this.element.querySelector('form').reset();
   }
 }
